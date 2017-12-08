47d08ec48badd5b898fa7486f3d678b2cd47f455
Pierre Jacquet <piejacquet@emi.u-bordeaux1.fr>
2017-12-08 04:43:18 +0100

    uPDATE JPP

--- js/bioinfoanim.js
+++ js/bioinfoanim.js
@@ -1,3 +1,6 @@
+var activeimage = $('#vehicle').attr("src");
+
+
 $('#feel-the-wave').wavify({
     height: 40,
     bones: 9,
@@ -22,36 +25,85 @@ $('#feel-the-wave-three').wavify({
     speed: .35
 });
 
-$( document ).ready(function() {
+$(document).ready(function () {
+    $("#personnage1").show();
+    $("#personnage2").show();
+    $('#past').on("click", function () {
+        var activeimage = $('#vehicle').attr("src");
+        $("#background").css('background-image', 'url(img/past.gif)');
+        $("#background").css('margin-top', '0px');
+        $("#personnage1").attr("src", "img/catcute.gif");
+        $("#personnage2").attr("src", "img/dog.gif");
+        $("#vehicle").attr("src", "img/knight.gif");
 
-$('#past').on("click", function () {
-    $("#background").css('background-image', 'url(img/past.gif)');
-    $("#vehicle").attr("src","img/knight.gif");
-    $('#vehicle').removeClass('vehicle3');
-    $('#vehicle').removeClass('vehicle2');
-    $('#vehicle').addClass('vehicle1');    
-    
+        $('#vehicle').removeClass('vehicle3');
+        $('#vehicle').removeClass('vehicle2');
+        $('#vehicle').addClass('vehicle1');
+
+        $("#personnage3").hide();
+        $("#personnage4").hide();
+        $("#personnage1").show();
+        $("#personnage2").show();
+        $("#personnage5").hide();
+        $("#personnage6").hide();
+
+        $('#vehicle').css({
+            bottom: 16 + "%",
+
+        });
     });
 
-$('#now').on("click", function () {
-    $("#background").css('background-image', 'url(img/now.gif)');
-    $("#vehicle").attr("src","img/delorean.png");
-    $('#vehicle').removeClass('vehicle3');
-    $('#vehicle').removeClass('vehicle1');
-    $('#vehicle').addClass('vehicle2');    
+    $('#now').on("click", function () {
+        var activeimage = $('#vehicle').attr("src");
+        $("#background").css('background-image', 'url(img/now.gif)');
+        $("#background").css('margin-top', '-35px');
+        $("#vehicle").attr("src", "img/delorean.png");
+
+        $("#personnage3").hide();
+        $("#personnage4").hide();
+        $("#personnage5").show();
+        $("#personnage6").show();
+        $("#personnage1").hide();
+        $("#personnage2").hide();
+
+
+
+        $('#vehicle').removeClass('vehicle3');
+        $('#vehicle').removeClass('vehicle1');
+        $('#vehicle').addClass('vehicle2');
+
+
+        $('#vehicle').css({
+            bottom: 7 + "%",
+            right: 150 + "px",
+        });
+
     });
 
-$('#futur').on("click", function () {
-    $("#background").css('background-image', 'url(img/futur.gif)');
-    $("#vehicle").attr("src","img/monkey.gif");
-    $('#vehicle').removeClass('vehicle2');
-    $('#vehicle').removeClass('vehicle1');
-    $('#vehicle').addClass('vehicle3');    
+    $('#futur').on("click", function () {
+        var activeimage = $('#vehicle').attr("src");
+        $("#background").css('background-image', 'url(img/futur2.gif)');
+        $("#background").css('margin-top', '0px');
+        $("#vehicle").attr("src", "img/monkey.gif");
+        $("#personnage1").hide();
+        $("#personnage2").hide();
+        $("#personnage5").hide();
+        $("#personnage6").hide();
+        $("#personnage3").show();
+        $("#personnage4").show();
+
+        $('#vehicle').removeClass('vehicle2');
+        $('#vehicle').removeClass('vehicle1');
+        $('#vehicle').addClass('vehicle3');
     });
 });
 
+
+
+
 $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
+    var activeimage = $('#vehicle').attr("src");
     top = scrollTop;
 
     $('#alcool').css({
@@ -60,10 +112,92 @@ $(window).scroll(function () {
     $('#beerlevel').css({
         bottom: 111 - $(window).scrollTop() * 0.2 + "px"
     });
-
     $('#vehicle').css({
-        left: $(window).scrollTop() * 0.8 + "px"
+        left: $(window).scrollTop() * 1.5 + "px"
     });
+
+    console.log(activeimage)
+    if ($(window).scrollTop() >= ($(document).height() - $(window).height())) {
+        $('#personnage1').css({
+            bottom: 232 - $(window).scrollTop() * 0.9 + "px",
+            right: 14 - $(window).scrollTop() * 0.02 + "%",
+            transform: "rotate(" + 90 + "deg)"
+        });
+        $('#personnage2').css({
+            bottom: 232 + $(window).scrollTop() * 1.5 + "px",
+            right: 19 - $(window).scrollTop() * 0.02 + "%",
+            transform: "rotate(" + 90 + "deg)"
+        });
+    }
+    if ($(window).scrollTop() >= ($(document).height() - $(window).height()) - 150) {
+
+        $('#personnage3').css({
+            bottom: 6 + $(window).scrollTop() * 0.009 + "%",
+            right: 24 - $(window).scrollTop() * 0.05 + "%",
+            transform: "rotate(" + 45 + "deg)"
+        });
+        $('#personnage4').css({
+            bottom: -11 + $(window).scrollTop() * 0.1 + "%",
+            right: 15 - $(window).scrollTop() * 0.04 + "%",
+            transform: "rotate(" + 90 + "deg)"
+        });
+    }
+
+    if ($(window).scrollTop() >= ($(document).height() - $(window).height()) - 150) {
+
+
+        $('#personnage6').css({
+            bottom: -11 + $(window).scrollTop() * 0.1 + "%",
+            right: 15 - $(window).scrollTop() * 0.04 + "%",
+            transform: "rotate(" + 90 + "deg)"
+        });
+    }
+
+
+
+    else {
+        $('#personnage1').css({
+            bottom: 232 + "px",
+            right: 14 + "%",
+            transform: "rotate(0)"
+
+        });
+        $('#personnage2').css({
+            bottom: 232 + "px",
+            right: 19 + "%",
+            transform: "rotate(0)"
+
+        });
+
+        $('#personnage3').css({
+            bottom: 6 + "%",
+            right: 24 + "%",
+            transform: "rotate(0)"
+
+        });
+        $('#personnage4').css({
+            bottom: -11 + "%",
+            right: 15 + "%",
+            transform: "rotate(0)"
+
+        });
+
+        $('#personnage6').css({
+            bottom: 7 + "%",
+            right: 15 + "%",
+            transform: "rotate(0)"
+
+        });
+
+
+    }
+
+    if (activeimage == "img/monkey.gif") {
+        $('#vehicle').css({
+            bottom: 5 - $(window).scrollTop() * 0.02 + "%",
+            right: 150 - $(window).scrollTop() * 0.2 + "px",
+        });
+    }
 
 })
 
