038fd1cfc053321d5eb511ffa966ca8a4d81c3c1
bockp <bockpeter@hotmail.com>
2017-12-08 03:48:23 +0100

    added JS list containing riddles and answers stored in 2 lists

---
+++ listeEnigmes.js
@@ -0,0 +1,95 @@
+let listeEnigmes = [
+
+
+    `<h1>petite charade pour debuter...</h1>
+<p>Mon premier est long et venimeux,</p>
+<p>Mon deuxieme est synonyme de secourir,</p>
+<p>Mon troisieme se cache sous ton lit,</p>
+<p>Prend les initiales de mes parts et tu aura ta reponse</p>
+`,
+    `<h1>Sam et la table ronde</h1>
+<p>Vous venez d'arriver en soirée, et vous etes le Sam designée de la soirée !</p>
+<p>Vos amis vous invitent a participer a un jeu a boire tres particulier, et, etant extremement poli, vous etes dans l'incapacitée de refuser !</p>
+<p>Le jeu consiste a se placer autour d'une table ronde. et comporte les régles suivantes:</p>
+<ul>
+  <li>Une personne désignée en avance, <b>dans ce cas-ci la personne numero 1</b>, commence en donnant une boisson fortement alcoolisée a son voisin le plus proche à gauche.</li>
+  <li>les gens ayant recu une boisson sont hors-jeux, puvant ni donner ni recevoir de boissons... et bien sûr trop ivre pour conduire !</li>
+  <li>Si une personne ne peut donner une boisson a son voisin le plus proche a gauche, il donne a la prochaine personne a gauche.</li>
+  <li>Si une personne est K.O, c'est le prochain a gauche qui prend le relais en donnat une boisson.</li>
+  <li>Quand il ne reste qu'une personne n'ayant pas bu, il/elle est declarée gagnant et </li>
+
+</ul>
+
+<p>En sachant que la personne 1 commence (et qu'il donne a la personne a sa gauche a lui), ou faut-il que vous vous placez pour gagner le jeu et pouvoir accomplir votre rôle sacrée de Sam?</p>
+<center><img src='images/enigme1.png' width='400' height='400' alt='emplacement autour de la table: >1->2->3->4->5->6->7->8->9-' /></center>
+
+<p>La réponse de cette enigme est le numero representant la personne qui gagnera le jeu dans l'image au-dessus.</p>
+
+
+`,
+    `<h1>L'enigme de Samstein</h1>
+<p>La soirée prend fin, et il faut trouver Sam, mais malheur !</p>
+
+<p>Vous avez tellement bu que vous avez oubliée qui etait le driver désignée ! Etait-ce <b>Bob</b>, <b>jean</b>, <b>morgane</b> ou <b>phillipine</b> ? Ils comptent tous sur vous pour decider qui conduit !</p>
+
+<p>Heureusement, quelques bribes de memoires vous reviennent. Vos collegues sont siége au Bar, comme visible dans l'image en dessous, et peuvent etre differenciée:</p>
+
+<p>par leur noms</p>
+<ul>
+  <li>Bob</li>
+  <li>Jean</li>
+  <li>Phillipine</li>
+  <li>Peter</li>
+</ul>
+<p>par leur position au bar (voir l'image en dessous)</p>
+<img src='images/enigme3.png' width='300' height='200' alt='emplacement au bar: 1--2--3--4' />
+
+<p>par leur porte clés</p>
+<ul>
+  <li>Crâne</li>
+  <li>Peace</li>
+  <li>Fleur</li>
+  <li>Nuit de l'Info</li>
+</ul>
+<p>par leur boisson</p>
+<ul>
+  <li>Martini</li>
+  <li>Eau</li>
+  <li>Vin</li>
+  <li>Vodka-cola</li>
+</ul>
+<p>par leur type de cigarettes (ou pas):</p>
+<ul>
+  <li>e-cigarette</li>
+  <li>ne fume pas</li>
+  <li>Marlboro</li>
+  <li>Blend</li>
+</ul>
+
+<p>Voici les quelques bribes que votre esprit corrompue par l'alcool a reussit a retenir:</p>
+
+<ul>
+  <li><b>Bob</b> boit du <b>Vodka-cola</b></li>
+  <li>La personne avec la <b>porte clé en forme de Crâne</b> est dans <b>la 4eme chaise au bar</b></li>
+  <li>La personne dans <b>la 4eme chaise du bar</b> fume des <b>Marlboros</b></li>
+  <li>La personne avec la<b> prote clé Fleur</b> est <b>directement a gauche</b> de celui avec <b>la porte clé Peace</b></li>
+  <li>La personne utilisant un <b>e-cigarette</b> est<b> juste a côté</b> de celui fumant des<b> Blends</b></li>
+  <li><b>Jean</b> est assis<b> juste à la droite</b> de la personne <b>buvant du Martini</b></li>
+  <li>La personne à la<b> place 3 </b>ne <b>fument pas de Blend</b></li>
+  <li><b>Jean</b> est assis <b>à la gauche</b> de la personne<b> buvant de l'eau</b></li>
+  <li>la personne avec la <b>porte clé Fleur</b> est assis<b> juste à gauche</b> de la personne qui<b> boit du Martini</b></li>
+  <li>La personne avec la <b>porte clé Peace</b> est <b>a gauche</b> de la personne <b>buvant du vodka-cola</b></li>
+  <li><b>Phillipine</b> est assise <b>juste à gauche</b> de la personne <b>buvant du vin</b></li>
+</ul>
+
+<p>Pouvez vous trouver qui est votre Sam de la soirée, celui qui n'a bu que de l'eau ?</p>
+
+<p>PS: on vous conseille d'avoir du papier et un crayon, pour prendre des notes.</p>
+`]
+
+
+let reponses = [
+    `sam`,
+    `7`,
+    `peter`
+]
